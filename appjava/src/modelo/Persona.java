06112483559ed516ad0db8c0a5b1d21193562d59
Carlos Andres Torres <caletorres_59@hotmail.com>
2016-09-14 20:40:41 -0500

    Crear clase persona
    se crea una persona .java con los siguientes atributos id,usuario clave
    
    tipo: nueva funcionalidad

---
+++ appjava/src/modelo/Persona.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Car
+ */
+public class Persona 
+{
+    String id;
+    String usuario;
+    String clave;
+
+    public Persona(String id, String usuario, String clave) {
+        this.id = id;
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+}
